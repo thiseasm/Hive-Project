453d2ed606fe3b24415f760f45ca8017e0ffbb51
ginisth <ginisth@hotmail.com>
2019-03-08 16:42:30 +0200

    Initializing MainMenu controller

--- HiveProject/Controllers/AccountController.cs
+++ HiveProject/Controllers/AccountController.cs
@@ -79,7 +79,8 @@ namespace HiveProject.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    //return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "MainMenu");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
---
+++ HiveProject/Controllers/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiveProject.Controllers
+{
+    [Authorize]
+    public class MainMenuController : Controller
+    {
+        // GET: MainMenu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
