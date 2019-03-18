b56cd180b06cc0a9dd8932cfe65ba7c169812f64
foivos petros kalfopoulos <zoskiacultus93@yahoo.gr>
2019-03-18 21:43:01 +0200

    Signal R Entry

---
+++ HiveProject/Chathub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using HiveProject.Interfaces;
+using HiveProject.Models;
+using HiveProject.Models.ChatSystem;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.SignalR;
+
+namespace HiveProject
+{
+    public class ChatHub : Hub
+    {
+        private readonly Interfaces.IStore _store;
+        private readonly UserManager<ApplicationUser> _manager;
+
+        public ChatHub(IStore store, UserManager<ApplicationUser> manager)
+        {
+            _manager = manager;
+            _store = store;
+        }
+
+        public async Task SendMessage(MessageViewModel model)
+        {
+            Message message = new Message
+            {
+                SenderId = model.SenderId,
+                ReceiverId = model.ReceiverId,
+                DateSent = model.Date.ToLocalTime(),
+                Body = model.Body,
+                Read = false
+            };
+
+            await _store.AddNewMessage();
+            model.Date = message.DateSent;
+            model.MessageId = message.MessageId;
+            model.hasBeenRead = message.Read;
+
+            await Clients.User(model.ReceiverId).SendAsync("ReceiveMessage", model);
+        }
+    }
+}
