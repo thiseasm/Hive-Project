31ee923a3334c0976249dae5eb11c2942b8d4acb
thiseasm <thiseas.matth@outlook.com>
2019-03-19 20:43:13 +0200

    Map now makes a vague appearence

---
+++ HiveProject/Scripts/hive.map.interaction.js
@@ -0,0 +1,29 @@
+﻿
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        return undefined;
+    }
+}
+
+function showPosition(position) {
+    var coordinates = [position.coordinates.latitude, position.coords.longitude]
+    return coordinates;
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            return "User denied the request for Geolocation.";
+        case error.POSITION_UNAVAILABLE:
+            return "Location information is unavailable.";
+        case error.TIMEOUT:
+            return "The request to get user location timed out.";
+        case error.UNKNOWN_ERROR:
+            return "An unknown error occurred.";
+    }
+}
+
+
+
