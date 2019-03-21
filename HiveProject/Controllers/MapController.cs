f0dedb90b61ebeca50df9271baef1fddf410bbfe
thiseasm <thiseas.matth@outlook.com>
2019-03-21 21:43:53 +0200

    Added controller for db.locations storage

--- HiveProject/Controllers/MapController.cs
+++ HiveProject/Controllers/MapController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using HiveProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using Microsoft.AspNet.Identity;
 using System.Web.Mvc;
 
 namespace HiveProject.Controllers
 {
     public class MapController : Controller
     {
-        // GET: Map
-        public ActionResult Index()
+        [HttpPost]
+        public void SaveLocation(List<decimal> coords)
         {
-            return View();
+            var thisUserId = User.Identity.GetUserId();
+
+            Location location = new Location()
+            {
+                Latitude = coords[0],
+                Longitude = coords[1],
+                Id = thisUserId
+            };
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var x = db.Locations.Add(location);
+                db.SaveChanges();
+            }
         }
     }
 }
