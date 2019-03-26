28593b1bde48f8566f0a297a32546251d3c26d3e
thiseasm <thiseas.matth@outlook.com>
2019-03-26 20:46:43 +0200

    All other locations now appear on the map aswell

--- HiveProject/Managers/InteractionManager.cs
+++ HiveProject/Managers/InteractionManager.cs
@@ -8,30 +8,39 @@ namespace HiveProject.Managers
 {
     public class InteractionManager
     {
-        public IEnumerable<Location> GetNearbyUsers(ApplicationUser User1)
+        public List<Location> GetNearbyUsers(ApplicationUser User1)
         {
-            IEnumerable<ApplicationUser> nearbyUsers = Enumerable.Empty<ApplicationUser>();
-
-            using (var db = new ApplicationDbContext())
-            {
-                nearbyUsers = db.Users.
-                    Where(u => u.Id != User1.Id
-                    && User1.CurrentLocation.Distance(u.CurrentLocation) <= User1.Radius)
-                    .ToList();
-            }
-            return GetLocations(nearbyUsers);
-        }
-
-        public IEnumerable<Location> GetLocations (IEnumerable<ApplicationUser> nearbyUsers)
-        {
-            IEnumerable<Location> nearbyLocations = Enumerable.Empty<Location>();
-
+            List<Location> nearbyUsers = new List<Location>();
             var db = new ApplicationDbContext();
-            nearbyLocations = db.Locations.
-                Where(loc => nearbyUsers.All(u => u.Id == loc.Id))
-                .ToList();
 
-            return nearbyLocations;
+            nearbyUsers = db.Locations.Where(loc => loc.Id != User1.Id).ToList();
+            return nearbyUsers;
         }
+
+        //public IEnumerable<Location> GetNearbyUsers(ApplicationUser User1)
+        //{
+        //    IEnumerable<ApplicationUser> nearbyUsers = Enumerable.Empty<ApplicationUser>();
+
+        //    using (var db = new ApplicationDbContext())
+        //    {
+        //        nearbyUsers = db.Users.
+        //            Where(u => u.Id != User1.Id
+        //            && User1.CurrentLocation.Distance(u.CurrentLocation) <= User1.Radius)
+        //            .ToList();
+        //    }
+        //    return GetLocations(nearbyUsers);
+        //}
+
+        //public IEnumerable<Location> GetLocations (IEnumerable<ApplicationUser> nearbyUsers)
+        //{
+        //    IEnumerable<Location> nearbyLocations = Enumerable.Empty<Location>();
+
+        //    var db = new ApplicationDbContext();
+        //    nearbyLocations = db.Locations.
+        //        Where(loc => nearbyUsers.All(u => u.Id == loc.Id))
+        //        .ToList();
+
+        //    return nearbyLocations;
+        //}
     }
 }
