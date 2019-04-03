9240648de6f4c24dfecc51000e4fa0d3838d7ab9
ginisth <ginisth@hotmail.com>
2019-04-03 20:41:47 +0300

    Fixing profile view

--- HiveProject/Controllers/AdminController.cs
+++ HiveProject/Controllers/AdminController.cs
@@ -1,18 +1,37 @@
-﻿using System;
+﻿using HiveProject.Models;
+using HiveProject.Viewmodels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace HiveProject.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var users = new List<UsersViewModel>();
+            using (var db = new ApplicationDbContext())
+            {
+               users = await db.Users.Where(x => x.Email != "Admin@admin.com")
+                                .Select(y => new UsersViewModel
+                                {
+                                    Id = y.Id,
+                                    Username = y.UserName,
+                                    Thumbnail = y.Thumbnail,
+                                    Age = y.Age,
+                                    Gender = y.UserGender,
+                                    Bio = y.Bio
+                                })
+                                .ToListAsync();
+            }
+            return View(users);
         }
     }
 }
